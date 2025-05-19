25db9c008978ec7fbbd7c5835ebb17e3b1964044
Ketan Dudka <ketandudka566123@gmail.com>
2025-05-19 12:36:30 +0530

    push

--- src/UseForm.jsx
+++ src/UseForm.jsx
@@ -1,12 +1,39 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
-const UseForm = () => {
+const UseForm = (formObj,validate,callback) => {
   const [values, setValues] = useState({});
   const [error, setError] = useState({});
 
+  const handleChange = (e)=>{
+    const {name,value} = e.target;
+
+    setValues({
+        ...values,
+        [name]:value
+    })
+  }
+
   const handleSubmit = (e) => {
     e.preventDefault();
+   setError(validate(values));
+
   };
+
+  useEffect(()=>{
+    if(Object.keys(error).length === 0){
+        callback();
+
+    }
+  },[error]);
+
+  return {
+    handleChange,
+    handleSubmit,
+    values,
+    setValues,
+    error,
+    setError
+  }
 };
 
 export default UseForm;
