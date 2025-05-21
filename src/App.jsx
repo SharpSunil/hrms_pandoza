97960dc4075b6c7b1887b239ea44239752084cd8
Ketan Dudka <ketandudka566123@gmail.com>
2025-05-21 11:56:12 +0530

    push

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,13 @@ import SendOtp from "./pages/SendOtp/SendOtp";
 import Login from "./pages/Login/Login";
 import ChangePassword from "./pages/ChangePasword/ChangePassword";
 import PrivateRoute from "./routes/PrivateRoute";
+import addEmp from "../src/pages/add_employee/AddEmp"
 
 function App() {
   const pages = [
     {
-      component: Login,
-      path: "/login",
+      component: addEmp,
+      path: "/add_employee",
       replace: true,
     },
     {
---
+++ src/pages/add_employee/AddEmp.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Page_Comp from '../../comp/page_comp/Page_Comp'
+
+const AddEmp = () => {
+  return (
+    <>
+      <Page_Comp/>
+    </>
+  )
+}
+
+export default AddEmp
