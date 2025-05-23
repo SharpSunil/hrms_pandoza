6f1ce53618e0a2174f814eddb85fefe0f3214898
Ketan Dudka <ketandudka566123@gmail.com>
2025-05-23 11:34:25 +0530

    push

--- src/comp/page_comp/Page_Comp.jsx
+++ src/comp/page_comp/Page_Comp.jsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Page_Comp.scss";
 import { Link } from "react-router-dom";
 import { RiDashboardFill } from "react-icons/ri";
 import { RiDashboardHorizontalLine } from "react-icons/ri";
 const Page_Comp = ({ children }) => {
+  const [active,setActive] = useState(false)
   const sideBarSection = [
     {
       icon: <RiDashboardFill />,
@@ -37,13 +38,17 @@ const Page_Comp = ({ children }) => {
         <div class="sidebar">
           <div class="logo"></div>
           <div class="nav_section">
-            {sideBarSection.map((item, index) => (
+            {/* {sideBarSection.map((item, index) => (
               <Link className="nav " to={item.path} key={index}>
                 <span className="fill_icon">{item.icon}</span>
                 <span className="line_icon">{item.line_icon}</span>
                 {item.path_name}
               </Link>
-            ))}
+            ))} */}
+
+            <Link className="nav" >
+             <RiDashboardHorizontalLine />
+            </Link>
           </div>
           <div class="logout"></div>
         </div>
