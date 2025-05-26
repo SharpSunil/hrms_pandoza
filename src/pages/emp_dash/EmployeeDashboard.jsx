dcc4f1a436453d3b81b18f66b7ef1aaa27649ec1
rishabhkhade <rishabhkhade95@gmail.com>
2025-05-26 11:50:43 +0530

    pp

---
+++ src/pages/emp_dash/EmployeeDashboard.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './EmployeeDashboard.scss'
+
+function EmployeeDashboard() {
+  return (
+    <>
+      <div className="parent">
+        <div className="cont">
+          <div className="emp_dash">
+            <h1>Employee Dashboard</h1>
+            <p>Welcome to your dashboard!</p>
+            <p>Here you can view your tasks, manage your profile, and more.</p>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default EmployeeDashboard
